9d32502428118cc7730110c8a6a1a288eec5ba22
Senk111 <akash.baral111@gmail.com>
2023-12-16 11:52:03 +0530

    1st Main2 PUSH

---
+++ Main2Jenkins/src/test/java/main2/CreateProductTest.java
@@ -0,0 +1,14 @@
+package main2;
+
+import org.testng.annotations.Test;
+
+public class CreateProductTest 
+{
+	@Test
+	public void createProductTest() 
+	{
+		System.out.println("Kphone 9");
+		
+	}
+
+}
---
+++ Main2Jenkins/src/test/java/main2/DeleteProductTest.java
@@ -0,0 +1,14 @@
+package main2;
+
+import org.testng.annotations.Test;
+
+public class DeleteProductTest 
+{
+	@Test
+	public void deleteProductTest() 
+	{
+		System.out.println("Product Deleted");
+		
+	}
+
+}
